22c8f8420f7bac0e79c2851fee7b94dcd68538f6
odi1n <sashf1999@mail.ru>
2022-12-13 21:52:18 +0300

    Добавлен конвертер изображений

---
+++ shop/utils.py
@@ -0,0 +1,16 @@
+from PIL import Image
+from pathlib import Path
+
+
+def convert_image_to_webp(image_path) -> str:
+    """
+    Convert image to webp format
+    """
+    image_path = Path(image_path)
+    image_name = image_path.stem
+    image_format = image_path.suffix[1:]
+
+    if image_format in ['png', 'jpg', 'jpeg']:
+        image = Image.open(image_path)
+        image.save(image_path.parent / (image_name + '.webp'), 'webp')
+        return image_name + '.webp'
